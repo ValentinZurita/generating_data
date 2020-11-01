fc3873a5633b92b7613039e68be0d07e9580883b
Valentin Alejandro <valentin.alejandro@icloud.com>
2020-11-01 16:56:39 -0600

    Cambios

--- skita_highs.py
+++ skita_highs.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 # Ruta del archivo
 filename = 'data/sitka_weather_07-2018_simple.csv'
@@ -10,22 +11,26 @@ with open(filename) as f:
     header_row = next(reader)
 
     # Obtener las temperaturas maximas del archivo
+    dates = []
     highs = []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
         highs.append(high)
+        dates.append(current_date)
         print(highs)
 
 # Graficar las temperaturas maximas.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # Dar formato a la grafica.
 plt.title('Temperaturas Maximas, Julio del 2018', fontsize=24)
 plt.xlabel('', fontsize=16)
 plt.ylabel('Temperatura (F)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
+fig.autofmt_xdate() # Dibuja las etiquetas del eje x en diagonal
 
 # Mostrar grafica.
 plt.show()
